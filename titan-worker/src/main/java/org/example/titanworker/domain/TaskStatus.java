25abec03942f8c89466d24910111b7dbe69909ae
CryBaby734 <frozengameno2@gmail.com>
2026-01-26 05:45:00 +0500

    docker compose updated

--- titan-worker/src/main/java/org/example/titanworker/domain/TaskStatus.java
+++ titan-worker/src/main/java/org/example/titanworker/domain/TaskStatus.java
@@ -3,6 +3,7 @@ package org.example.titanworker.domain;
 public enum TaskStatus {
     PENDING,     // Ждет, пока выполнятся родители
     READY,       // Родителей нет (или они SUCCESS), можно брать в работу
+    QUEUED,
     IN_PROGRESS, // Воркер трудится
     SUCCESS,     // Готово
     FAILED       // Ошибка
